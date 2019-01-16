9ab5dc1fee9b38d37295f3ca84e89e4f8c3ab40d
Guddu Chauhan <gc4764@gmail.com>
2019-01-16 21:28:55 +0530

    signals.py partial complete

---
+++ config.py
@@ -0,0 +1,12 @@
+
+local_server=True,
+local_uri= "",
+
+public_token= "CH9fntrriTjlh2g9mKj6Cxu5LBJ33WKc"
+access_token ="M0qPCw2xFsj6i1kKZlgapDJZoCrVN28p"
+userid ='ZT8569'
+
+db_user ='root'
+db_password ='1124'
+db_host = 'localhost'
+db_name="test"
---
+++ test.py
@@ -0,0 +1,19 @@
+
+
+from time import sleep
+from datetime import datetime ,timedelta
+
+
+now = datetime.today()
+
+print(now.isoweekday())
+
+
+
+from_datetime = str(datetime.today() - timedelta(days=300))
+# print(from_datetime)
+
+
+
+
+
